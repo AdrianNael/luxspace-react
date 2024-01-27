b1437b1ff5af719cb2f7f3e18af32d34689ea871
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2024-01-27 07:44:41 +0700

    Homepages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import 'assets/css//app.css'
+import 'assets/css/app.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from 'pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-xl text-'>Hero </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
