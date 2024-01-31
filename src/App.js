4a1172667a6018292cf66481209cdc0db2b0920d
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2024-01-31 15:09:33 +0700

    Update Prdouct Detail  & Suggestion

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Details from "pages/Details";
 import Cart from "pages/Cart";
 import Congratulation from 'pages/Congratulation'
 import NotFound from 'pages/NotFound'
+import Provider from 'helpers/hooks/useGlobalContext'
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<HomePage/>} />{" "}
-          <Route path="/categories/:idc" element={<Details/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/congratulation" element={<Congratulation/>} />
-          <Route path="*" element={<NotFound/>} />
-
-        </Routes>
+        <Provider>
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/categories/:idc" element={<Details />} />
+            <Route path="/categories/:idc/products/:idp" element={<Details />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/congratulation" element={<Congratulation />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Provider>
       </Router>
     </div>
   );
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,25 +1,131 @@
 import React from "react";
 
+import { useParams } from "react-router-dom";
+
 import Header from "parts/Header";
 import Breadcrumb from "components/Breadcrumb";
-import Sitemap from "parts/HomePage/Sitemap";
-import Footer from "parts/Footer";
+
 import ProductDetails from "parts/Details/ProductDetails";
 import Suggestion from "parts/Details/Suggestion";
 
+import Sitemap from "parts/HomePage/Sitemap";
+import Footer from "parts/Footer";
+
+import useAsync from "helpers/hooks/useAsync";
+import fetch from "helpers/fetch";
+
+function LoadingProductDetails() {
+  return (
+    <section className="container mx-auto">
+      <div className="flex flex-wrap my-4 md:my-12">
+        <div className="w-full px-4 md:hidden">
+          <div className="h-16 bg-gray-300 rounded-full w-80 animate-pulse"></div>
+          <div className="w-40 h-4 bg-gray-300 rounded-full animate-pulse"></div>
+        </div>
+        <div className="flex-1">
+          <div className="slider">
+            <div className="thumbnail">
+              {Array(5)
+                .fill()
+                .map((_, index) => {
+                  return (
+                    <div className="relative px-4 card group" key={index}>
+                      <div
+                        className="bg-gray-300 rounded-xl item animate-pulse"
+                        style={{ width: 106, height: 106 }}
+                      ></div>
+                    </div>
+                  );
+                })}
+            </div>
+            <div className="preview">
+              <div className="h-full overflow-hidden rounded-lg item">
+                <div className="h-full overflow-hidden bg-gray-300 rounded-lg item animate-pulse"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 px-4 md:p-6">
+          <div className="h-16 bg-gray-300 rounded-full w-80 animate-pulse"></div>
+          <div className="w-40 h-4 mt-4 bg-gray-300 rounded-full animate-pulse"></div>
+
+          <div className="h-10 mt-8 bg-gray-300 rounded-full w-44 animate-pulse"></div>
+
+          <hr className="my-8" />
+
+          <div className="h-4 mt-6 bg-gray-300 rounded-full w-36 animate-pulse"></div>
+          <div className="h-4 mt-6 bg-gray-300 rounded-full w-88 animate-pulse"></div>
+          <div className="w-40 h-4 mt-6 bg-gray-300 rounded-full animate-pulse"></div>
+          <div className="h-4 mt-6 bg-gray-300 rounded-full w-96 animate-pulse"></div>
+          <div className="w-64 h-4 mt-6 bg-gray-300 rounded-full animate-pulse"></div>
+          <div className="h-4 mt-6 bg-gray-300 rounded-full w-88 animate-pulse"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function LoadingSuggestions() {
+  return (
+    <section class="bg-gray-100 px-4 py-16">
+      <div class="container mx-auto">
+        <div class="flex flex-start mb-4">
+          <h3 class="text-2xl capitalize font-semibold">
+            Complete your room <br class="" />
+            with what we designed
+          </h3>
+        </div>
+        <div class="flex overflow-x-auto mb-4 -mx-3">
+          {Array(4)
+            .fill()
+            .map((index) => {
+              return (
+                <div class="px-3 flex-none" style={{ width: 320 }} key={index}>
+                  <div class="rounded-xl p-4 pb-8 relative bg-white">
+                    <div class="rounded-xl overflow-hidden card-shadow w-full h-36">
+                      <div
+                        className="h-full overflow-hidden bg-gray-300 rounded-lg item animate-pulse"
+                        style={{ width: 287, height: 150 }}
+                      ></div>
+                    </div>
+                    <div className="w-56 h-4 mt-6 bg-gray-300 rounded-full animate-pulse"></div>
+                    <div className="w-40 h-4 mt-3 bg-gray-300 rounded-full animate-pulse"></div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
+}
+
 export default function HomePages() {
+  const { idp } = useParams();
+  const { data, run, isLoading } = useAsync();
+
+  React.useEffect(() => {
+    run(fetch({ url: `/api/products/${idp}` }));
+  }, [run, idp]);
+
   return (
     <>
       <Header theme="black" />
       <Breadcrumb
-        List={[
+        list={[
           { url: "/", name: "Home" },
           { url: "/categories/91231", name: "Office Room" },
           { url: "/categories/91231/products/7888", name: "Details" },
         ]}
       />
-      <ProductDetails />
-      <Suggestion />
+
+      {isLoading ? <LoadingProductDetails /> : <ProductDetails data={data} />}
+      {isLoading ? (
+        <LoadingSuggestions />
+      ) : (
+        <Suggestion data={data?.relatedProducts || {}} />
+      )}
+
       <Sitemap />
       <Footer />
     </>
