49c3ec7d61e99918aed3dd0a7e1f31d2b49b242d
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2024-02-01 13:40:10 +0700

    Fix Cart Icon

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -67,22 +67,22 @@ function LoadingProductDetails() {
 
 function LoadingSuggestions() {
   return (
-    <section class="bg-gray-100 px-4 py-16">
-      <div class="container mx-auto">
-        <div class="flex flex-start mb-4">
-          <h3 class="text-2xl capitalize font-semibold">
-            Complete your room <br class="" />
+    <section className="bg-gray-100 px-4 py-16">
+      <div className="container mx-auto">
+        <div className="flex flex-start mb-4">
+          <h3 className="text-2xl capitalize font-semibold">
+            Complete your room <br className="" />
             with what we designed
           </h3>
         </div>
-        <div class="flex overflow-x-auto mb-4 -mx-3">
+        <div className="flex overflow-x-auto mb-4 -mx-3">
           {Array(4)
             .fill()
             .map((index) => {
               return (
-                <div class="px-3 flex-none" style={{ width: 320 }} key={index}>
-                  <div class="rounded-xl p-4 pb-8 relative bg-white">
-                    <div class="rounded-xl overflow-hidden card-shadow w-full h-36">
+                <div className="px-3 flex-none" style={{ width: 320 }} key={index}>
+                  <div className="rounded-xl p-4 pb-8 relative bg-white">
+                    <div className="rounded-xl overflow-hidden card-shadow w-full h-36">
                       <div
                         className="h-full overflow-hidden bg-gray-300 rounded-lg item animate-pulse"
                         style={{ width: 287, height: 150 }}
