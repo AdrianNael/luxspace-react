ea41f76d15b5fd12d196f8f16fe102e4a1dd8878
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2024-02-01 23:20:53 +0700

    Update BrowseRoom

--- src/parts/HomePage/BrowseRoom.js
+++ src/parts/HomePage/BrowseRoom.js
@@ -1,136 +1,106 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import Carousel from "../../components/Carousel";
 import useAsync from "../../helpers/hooks/useAsync";
 import fetch from "../../helpers/fetch";
 
-function Loading({ ratio = {} }) {
-
-// pembuatan skelton BrowseRoom
-  const dummy = [
-    {
-      id: 1,
-      ratio: {
-        default: "1/9",
-        md: "1/4",
-      },
-    },
-    {
-      id: 2,
-      ratio: {
-        default: "1/9",
-        md: "2/2",
-      },
-    },
-    {
-      id: 3,
-      ratio: {
-        default: "1/9",
-        md: "2/3",
-      },
-    },
-    {
-      id: 4,
-      ratio: {
-        default: "1/9",
-        md: "1/4",
-      },
-    },
-  ];
-//lanjutan dari skelton
-  return dummy.map((item, index) => {
-    return (
-      <div
-        key={item.id}
-        className={`relative card ${
-          ratio?.wrapper.default?.[item.ratio.default]
-        } ${ratio?.wrapper.md?.[item.ratio.md]}`}
-        style={{ height: index === 0 ? 180 : "auto" }}
-      >
-        <div className="w-full h-full bg-gray-300 rounded-lg">
-          <div className={`overlay ${ratio?.meta?.[item.ratio.md]}`}>
-            <div className="w-24 h-3 mt-3 bg-gray-400 rounded-full"></div>
-            <div className="h-3 mt-2 bg-gray-400 rounded-full w-36"></div>
-          </div>
+function Loading() {
+  return Array(6)
+    .fill()
+    .map((_, index) => {
+      return (
+        <div className="relative px-4 card group" key={index}>
+          <div
+            className="relative overflow-hidden bg-gray-300 rounded-xl card-shadow"
+            style={{ width: 287, height: 386 }}
+          ></div>
+          <div className="w-24 h-3 mt-3 bg-gray-300 rounded-full"></div>
+          <div className="h-3 mt-2 bg-gray-300 rounded-full w-36"></div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
 }
 
-export default function BrowseRoom() {
-  const { data, run, isLoading } = useAsync();
+export default function JuastArrived() {
+  const { data, error, run, isLoading } = useAsync();
+  const refContainer = useRef(null);
+
   useEffect(() => {
-    run(fetch({ url: "/api/categories/?page=1&limit=4" }));
+    run(fetch({ url: "/api/products/?page=1&limit=10" }));
   }, [run]);
 
-  const ratioClassNames = {
-    wrapper: {
-      default: {
-        "1/9": "col-span-9 row-span-1",
-      },
-      md: {
-        "1/4": "md:col-span-4 md:row-span-1",
-        "2/2": "md:col-span-2 md:row-span-2",
-        "2/3": "md:col-span-3 md:row-span-2",
-      },
-    },
-    meta: {
-      "1/9":
-        "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
-      "1/4":
-        "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
-      "2/2":
-        "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
-      "2/3":
-        "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
-    },
+  const scrollToTop = () => {
+    // Scroll ke atas halaman
+    window.scrollTo(0, 0);
   };
 
   return (
-    <section className="flex px-4 py-16 bg-gray-100" id="browse-the-room">
-      <div className="container mx-auto">
-        <div className="flex mb-4 flex-start">
+    <section className="flex flex-col py-16">
+      <div className="container mx-auto mb-4">
+        <div className="flex justify-center mb-4 text-center">
           <h3 className="text-2xl font-semibold capitalize">
-            browse the room <br className="" />
-            that we designed for you
+            Just Arrived <br className="" />
+            this summer for you
           </h3>
         </div>
-
-        <div className="grid grid-cols-9 grid-rows-2 gap-4">
-          {isLoading ? (
-            <Loading ratio={ratioClassNames} />
-          ) : (
-            data.data.map((item, index) => {
+      </div>
+      <div className="px-4 overflow-x-hidden" id="carousel">
+        <div className="container mx-auto" ref={refContainer}></div>
+        {isLoading ? (
+          <div
+            className="relative flex flex-row -mx-4"
+            style={{
+              paddingLeft:
+                refContainer.current?.getBoundingClientRect?.()?.left - 16 || 0,
+            }}
+          >
+            <Loading />{" "}
+          </div>
+        ) : error ? (
+          JSON.stringify(error)
+        ) : data.data.length === 0 ? (
+          "No Product Found"
+        ) : (
+          <Carousel refContainer={refContainer}>
+            {data.data.map((item) => {
               return (
-                <div
-                  key={item.id}
-                  className={`relative card ${
-                    ratioClassNames?.wrapper.default?.[item.ratio.default]
-                  } ${ratioClassNames?.wrapper.md?.[item.ratio.md]}`}
-                  style={{ height: index === 0 ? 180 : "auto" }}
-                >
-                  <div className="card-shadow rounded-xl">
+                <div className="relative px-4 card group" key={item.id}>
+                  <div
+                    className="relative overflow-hidden rounded-xl card-shadow"
+                    style={{ width: 287, height: 386 }}
+                  >
+                    <div className="absolute flex items-center justify-center w-full h-full transition duration-200 bg-black opacity-0 group-hover:opacity-100 bg-opacity-35">
+                      <div className="flex items-center justify-center w-16 h-16 text-black bg-white rounded-full">
+                        <svg
+                          className="fill-current"
+                          width="43"
+                          height="24"
+                          viewBox="0 0 43 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          {/* SVG path */}
+                        </svg>
+                      </div>
+                    </div>
                     <img
-                      src={`/images/content/${item.imageUrl}`}
-                      alt={item.title}
-                      className="object-cover object-center w-full h-full overflow-hidden overlay rounded-xl"
+                      src={item.imageUrl}
+                      alt=""
+                      className="object-cover object-center w-full h-full"
                     />
                   </div>
-                  <div
-                    className={`overlay ${
-                      ratioClassNames?.meta?.[item.ratio.md]
-                    }`}
-                  >
-                    <h5 className="text-lg font-semibold">{item.title}</h5>
-                    <span className="">
-                      {item.products} item{item.products > 1 ? "s" : ""}
-                    </span>
-                  </div>
+                  <h5 className="mt-4 text-lg font-semibold">{item.title}</h5>
+                  <span className="">IDR {item.price}</span>
+                  <Link
+                    to={`/categories/${item.idc}/products/${item.id}`}
+                    className="stretched-link"
+                    onClick={scrollToTop}
+                  ></Link>
                 </div>
               );
-            })
-          )}
-        </div>
+            })}
+          </Carousel>
+        )}
       </div>
     </section>
   );
